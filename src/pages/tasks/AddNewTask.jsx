3a5a90f8fbf56ea84fb409656d912609cb4b9ea1
Ali Khattab <ali.khattab@digital.homeoffice.gov.uk>
2023-04-01 21:16:17 +0100

    adding fetch request for user tasks

---
+++ src/pages/tasks/AddNewTask.jsx
@@ -0,0 +1,42 @@
+import { Button, useColorMode } from "@chakra-ui/react";
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
+import './datepicker.css';
+
+import { useAxios } from "../../utils/hooks";
+
+const AddNewTask = () => {
+
+  const axiosInstance = useAxios();
+  const [dueDate, setDueDate] = useState(new Date());
+
+
+  const addTask = (newTask) => {
+
+    console.log('new task = ', newTask)
+    axiosInstance.post(`/tasks/new`, newTask)
+    // console.log(dayjs().format())
+    // axiosInstance.get(`/tasks/user/abc`)
+  }
+
+  const dummyTask = {
+    dueDate: dayjs(dueDate).format(),
+    complete: true
+  }
+
+  const isLight = useColorMode().colorMode==='light';//you can check what theme you are using right now however you want
+
+
+  return (
+    <div className={isLight?"light-theme":"dark-theme"}>
+      <DatePicker selected={dueDate} onChange={(date) => setDueDate(date)} showTimeSelect />
+
+      <Button onClick={() => { addTask(dummyTask) }}>
+        Click to add task here
+      </Button>
+    </div>);
+}
+
+export default AddNewTask;
--- src/routes/MyTasks.jsx
+++ src/routes/MyTasks.jsx
@@ -1,11 +1,13 @@
 import { Heading } from '@chakra-ui/react'
-import UserTasks from "./UserTasks";
+import AddNewTask from '../pages/tasks/AddNewTask';
+import UserTasks from "../pages/tasks/UserTasks";
 
 const MyTasks = () => {
   return (
     <>
     <Heading>This is my tasks</Heading>
-    <UserTasks />
+      <UserTasks />
+      <AddNewTask/>
     </>
   );
 }
